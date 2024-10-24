66a9eb7be5c541b9b64cc7e41d8c9421d10807bd
tonysousa18 <167309815+tonysousa18@users.noreply.github.com>
2024-10-24 18:10:04 -0300

    Update 1174.py
    Faça um programa que leia um vetor A[100]. No final, mostre todas as posições do vetor que armazenam um valor menor ou igual a 10 e o valor armazenado em cada uma das posições.

--- 1174.py
+++ 1174.py
@@ -1,3 +1,7 @@
+#Faça um programa que leia um vetor A[100]. 
+#No final, mostre todas as posições do vetor que armazenam um valor menor ou igual a 10 e o valor armazenado em cada uma das posições.
+
+
 lista = list(range(100))
 
 for i in lista:
